120b5bd0f843306d84ba73399d5a62cbd32b2dde
p-zach <porterdzach@gmail.com>
2026-01-13 17:18:29 -0500

    Wrap function in try-except

--- function_app.py
+++ function_app.py
@@ -6,15 +6,15 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="test", methods=["GET"])
 def test(req: func.HttpRequest) -> func.HttpResponse:
-    container = get_container("facts")
-    items = list(
-        container.query_items(
+    try:
+        container = get_container("facts")
+        items = list(container.query_items(
             query="SELECT * FROM c",
             enable_cross_partition_query=True
+        ))
+        return func.HttpResponse(json.dumps(items), mimetype="application/json")
+    except Exception as e:
+        return func.HttpResponse(
+            str(e),
+            status_code=500
         )
-    )
-
-    return func.HttpResponse(
-        json.dumps(items),
-        mimetype="application/json"
-    )
