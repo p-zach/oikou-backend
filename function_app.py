ce90183140eb8af8cd0ab8791b51feffeb0d629b
p-zach <porterdzach@gmail.com>
2026-01-13 20:38:21 -0500

    Test separate file

--- function_app.py
+++ function_app.py
@@ -1,7 +1,8 @@
 import azure.functions as func
+from shared.cosmos_client import return_5
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="test", methods=["GET"])
 def test(req: func.HttpRequest) -> func.HttpResponse:
-    return func.HttpResponse("Hello, world!", status_code=200)
+    return func.HttpResponse("Hello, world! " + str(return_5()), status_code=200)
