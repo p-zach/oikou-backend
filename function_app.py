4f4c2251a0b0399ef892a754727122b70e35a77f
p-zach <porterdzach@gmail.com>
2026-01-13 21:43:40 -0500

    Try accessing Cosmos

--- function_app.py
+++ function_app.py
@@ -6,16 +6,15 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="test", methods=["GET"])
 def test(req: func.HttpRequest) -> func.HttpResponse:
-    # container = get_container("facts")
-    # items = list(
-    #     container.query_items(
-    #         query="SELECT * FROM c",
-    #         enable_cross_partition_query=True
-    #     )
-    # )
+    container = get_container("facts")
+    items = list(
+        container.query_items(
+            query="SELECT * FROM c",
+            enable_cross_partition_query=True
+        )
+    )
 
     return func.HttpResponse(
-        # json.dumps(items),
-        '{"val": "test with importing get_container"}',
+        json.dumps(items),
         mimetype="application/json"
     )
