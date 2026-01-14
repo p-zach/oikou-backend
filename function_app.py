6108482e3d6fb7741b14fa3350e9ee11c2cd234a
p-zach <porterdzach@gmail.com>
2026-01-13 20:32:27 -0500

    Remove everything to isolate problem

--- function_app.py
+++ function_app.py
@@ -1,23 +1,7 @@
 import azure.functions as func
-from shared.cosmos_client import get_container
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="test", methods=["GET"])
 def test(req: func.HttpRequest) -> func.HttpResponse:
     return func.HttpResponse("Hello, world!", status_code=200)
-
-@app.route(route="test_cosmos", methods=["GET"])
-def test_cosmos(req: func.HttpRequest) -> func.HttpResponse:
-    try:
-        container = get_container("facts")
-        items = list(container.query_items(
-            query="SELECT * FROM c",
-            enable_cross_partition_query=True
-        ))
-        return func.HttpResponse(items[0]["id"])
-    except Exception as e:
-        return func.HttpResponse(
-            str(e),
-            status_code=500
-        )
